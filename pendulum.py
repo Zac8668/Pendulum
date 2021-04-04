02f60e518aa2ca9bf6fe807b0fc16bfb10a829ea
Zac8668 <78173025+Zac8668@users.noreply.github.com>
2021-04-04 01:09:53 -0300

    Added double pendulum

--- pendulum.py
+++ pendulum.py
@@ -2,13 +2,15 @@ from dataclasses import dataclass
 from math import pi, sin, cos
 
 class Pendulum:
-    def __init__(self, pivot:list, length, angle:float = 0, gravity = -0.01):
+    def __init__(self, pivot:list, length, angle:float = 0, gravity = -0.03, friction = 0.96):
         self.pivot = pivot
         self.length = length
         self.angle = angle
         self.bob = self.bob_pos()
         
         self.gravity = gravity
+        self.friction = friction
+        
         self.angle_velocity = 0
         self.angle_acceleration = 0
         
@@ -33,12 +35,105 @@ class Pendulum:
         self.angle += self.angle_velocity
         self.angle_velocity += self.angle_acceleration
         
-        self.angle_velocity *= 0.99
+        self.angle_velocity *= self.friction
         
         self.bob = self.bob_pos()
 
     def update(self):
         self.check_angle()
 
+        self.check_physics()
+        
+class DoublePendulum:
+    def __init__(self, pivot:list, length, angle:float = 0, gravity = 1, friction = 0.96):
+        self.pivot = pivot
+        self.length1 = length
+        self.angle1 = angle
+        self.bob1 = self.bob1_pos()
+        self.mass1 = 10
+        self.velocity1 = 0
+        self.acceleration1 = 0
+        
+        self.length2 = length
+        self.angle2 = angle
+        self.bob2 = self.bob2_pos()
+        self.mass2 = 10
+        self.velocity2 = 0
+        self.acceleration2 = 0
+        
+        self.GRAVITY = gravity
+        self.FRICTION = friction
+        
+    def bob1_pos(self):
+        x = self.length1 * sin(self.angle1) + self.pivot[0]
+        y = self.length1 * cos(self.angle1) + self.pivot[1]
+
+        bob_pos = [x, y]
+        return bob_pos
+    
+    def bob2_pos(self):
+        x = self.length2 * sin(self.angle2) + self.bob1[0]
+        y = self.length2 * cos(self.angle2) + self.bob1[1]
+
+        bob_pos = [x, y]
+        return bob_pos
+    
+    def check_angle(self):
+        if self.angle1 > pi * 2:
+            self.angle1 -= pi * 2
+        elif self.angle1 < 0:
+            self.angle1 += pi * 2
+        
+        if self.angle2 > pi * 2:
+            self.angle2 -= pi * 2
+        elif self.angle2 < 0:
+            self.angle2 += pi * 2
+            
+        self.bob1 = self.bob1_pos()
+        self.bob2 = self.bob2_pos()
+        
+    def calc_acc1(self):
+        dividend1 = -self.GRAVITY * (2 * self.mass1 + self.mass2) * sin(self.angle1)
+        dividend2 = -self.mass2 * self.GRAVITY * sin(self.angle1 - 2 * self.angle2)
+        dividend3 = -2 * sin(self.angle1 - self.angle2) * self.mass2
+        dividend4 = self.velocity2 ** 2 * self.length2 + self.velocity1 ** 2 * self.length1 * cos(self.angle1 - self.angle2)
+        
+        dividend = dividend1 + dividend2 + dividend3 * dividend4
+        
+        divisor = self.length1 * (2 * self.mass1 + self.mass2 - self.mass2 * cos(2 * self.angle1 - 2 * self.angle2))
+        
+        return dividend / divisor
+    
+    def calc_acc2(self):
+        dividend1 = 2 * sin(self.angle1 - self.angle2)
+        dividend2 = self.velocity1 ** 2 * self.length1 * (self.mass1 * self.mass2)
+        dividend3 = self.GRAVITY * (self.mass1 + self.mass2) * cos(self.angle1)
+        dividend4 = self.velocity2 ** 2 * self.length2 * self.mass2 * cos(self.angle1 - self.angle2)
+        
+        dividend = dividend1 * (dividend2 + dividend3 + dividend4)
+        
+        divisor = self.length2 * (2 * self.mass1 + self.mass2 - self.mass2 * cos(2 * self.angle1 - 2 * self.angle2))
+        
+        return dividend / divisor
+        
+    def check_physics(self):
+        self.acceleration1 = self.calc_acc1()
+        
+        self.velocity1 += self.acceleration1
+        self.angle1 += self.velocity1
+        
+        self.acceleration2 = self.calc_acc2()
+        
+        self.velocity2 += self.acceleration2
+        self.angle2 += self.velocity2
+        
+        self.velocity1 *= self.FRICTION
+        self.velocity2 *= self.FRICTION
+        
+        self.bob1 = self.bob1_pos()
+        self.bob2 = self.bob2_pos()
+
+    def update(self):
+        self.check_angle()
         self.check_physics()
         
--- render.py
+++ render.py
@@ -1,7 +1,7 @@
 import sys
 
 import pygame
-from pendulum import Pendulum
+from pendulum import Pendulum, DoublePendulum
 from math import pi
 
 class Render:
@@ -11,10 +11,10 @@ class Render:
         self.screen_res = [512,512]
         self.clock = pygame.time.Clock()
         self.screen = pygame.display.set_mode(self.screen_res)
-        self.fps = 30
+        self.fps = 60
         
         pivot = [self.screen_res[0] / 2, self.screen_res[1] / 4]
-        self.pend = Pendulum(pivot, 100, pi - 0.001)
+        self.pend = DoublePendulum(pivot, 100, pi / 4)
         
     def draw_text(self, text:str, pos):
         text_img = pygame.font.SysFont('Calibri', 20).render(text, True, (255,255,255))
@@ -31,30 +31,40 @@ class Render:
                 elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-                    
-            pressed = pygame.key.get_pressed()
-            
-            if pressed[pygame.K_UP]:
-                self.pend.angle_velocity += 0.05
-                self.pend.update()
-                
-            if pressed[pygame.K_DOWN]:
-                self.pend.angle_velocity -= 0.05
-                self.pend.update()
 
             self.screen.fill((0, 0, 0))
+            
+            pressed = pygame.key.get_pressed()
+            
+            if pressed[pygame.K_a]:
+                self.pend.velocity1 -= 0.1
+                
+            if pressed[pygame.K_d]:
+                self.pend.velocity1 += 0.1
+                
+            if pressed[pygame.K_LEFT]:
+                self.pend.velocity2 -= 0.1
+                
+            if pressed[pygame.K_RIGHT]:
+                self.pend.velocity2 += 0.1
             
             #update screen
             self.pend.update()
             
             #draw some text
-            self.draw_text(f'angle: {self.pend.angle:.6f}', [10, 10])
-            self.draw_text(f'angle acceleration: {self.pend.angle_acceleration:.6f}', [10, 30])
-            self.draw_text(f'angle velocity: {self.pend.angle_velocity:.6f}', [10, 50])
+            self.draw_text(f'angle1: {self.pend.angle1:.6f}', [10, 10])
+            self.draw_text(f'angle1 acceleration: {self.pend.acceleration1:.6f}', [10, 30])
+            self.draw_text(f'angle1 velocity: {self.pend.velocity1:.6f}', [10, 50])
+            self.draw_text(f'angle2: {self.pend.angle2:.6f}', [10, 70])
+            self.draw_text(f'angle2 acceleration: {self.pend.acceleration2:.6f}', [10, 90])
+            self.draw_text(f'angle2 velocity: {self.pend.velocity2:.6f}', [10, 110])
+            self.draw_text(f'fps: {self.clock.get_fps():.6f}', [10, 130])
             
             #draw pendulum
-            pygame.draw.line(self.screen, (255, 255, 255), self.pend.pivot, [self.pend.bob[0] - 1, self.pend.bob[1]], 3)
-            pygame.draw.circle(self.screen, (255, 255, 255), self.pend.bob, 8)
+            pygame.draw.line(self.screen, (255, 255, 255), [self.pend.pivot[0] - 1, self.pend.pivot[1]], [self.pend.bob1[0] - 1, self.pend.bob1[1]], 3)
+            pygame.draw.circle(self.screen, (255, 255, 255), self.pend.bob1, 8)
+            pygame.draw.line(self.screen, (255, 255, 255), [self.pend.bob1[0] - 1, self.pend.bob1[1]], [self.pend.bob2[0] - 1, self.pend.bob2[1]], 3)
+            pygame.draw.circle(self.screen, (255, 255, 255), self.pend.bob2, 8)
             
             #update screen
             pygame.display.flip()
